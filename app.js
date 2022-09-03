f62f2a38069d19a23731da27a4a18c10923f17a0
Daniel <danielo.visage@gmail.com>
2022-09-03 18:19:15 +0200

    add user queries

--- app.js
+++ app.js
@@ -1,26 +1,30 @@
-const express = require('express');
-const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 
-const db = require('./queries')
-const port = 4000
+const db = require("./queries");
+const port = 4000;
 
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get("/", (request, response) => {
+  response.json({ info: "Node.js, Express, Postgres API" });
+});
+app.set("port", process.env.PORT || port);
+app.get("/posts", db.getPosts);
+app.get("/posts/:id", db.getPostById);
+app.post("/posts", db.createPost);
+app.delete("/posts/:id", db.deletePost);
 
-app.get('/', (request, response) => {
-    response.json({info: 'Node.js, Express, Postgres API'})
-})
-app.set('port', process.env.PORT || port);
-app.get('/posts', db.getPosts);
-app.get('/posts/:id', db.getPostById);
-app.post('/posts', db.createPost);
-app.delete('/posts/:id', db.deletePost);
+app.get("/users", db.getUsers);
+app.get("/users/:id", db.getUserById);
+app.post("/users", db.createUser);
+app.delete("/users/:id", db.deleteUser);
 
 app.listen(4000, function () {
-    console.log(`Server is running port ${port}`);
+  console.log(`Server is running port ${port}`);
 });
--- queries.js
+++ queries.js
@@ -1,56 +1,109 @@
-const Pool = require('pg').Pool
+const Pool = require("pg").Pool;
 const pool = new Pool({
-  user: 'daniel',
-  host: 'localhost',
-  database: 'chatblog_db',
-  password: 'root',
+  user: "daniel",
+  host: "localhost",
+  database: "chatblog_db",
+  password: "root",
   port: 5432,
-})
+});
 
 const getPosts = (request, response) => {
-    pool.query('SELECT * FROM posts ORDER BY id ASC', (error, results) => {
-        if (error) {
-            throw error
-        }
-        response.status(200).json(results.rows)
-    })
-}
+  pool.query("SELECT * FROM posts ORDER BY id ASC", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
 
 const getPostById = (request, response) => {
-    const id = parseInt(request.params.id)
-    pool.query('SELECT * FROM posts WHERE id = $1', [id], (error, results) => {
-        if (error) {
-            throw error
-        }
-        response.status(200).json(results.rows)
-    })
-}
+  const id = parseInt(request.params.id);
+  pool.query("SELECT * FROM posts WHERE id = $1", [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
 
 const createPost = (request, response) => {
-    const {id, title, content, image} = request.body
+  const { id, title, content, image } = request.body;
 
-    pool.query('INSERT INTO posts (id, title, content, image) VALUES ($1, $2, $3, $4) RETURNING *', [id, title, content, image], (error, results) => {
-        if (error) {
-            throw error
-        }
-        response.status(201).send(`Post added with ID: ${results.rows[0].id}`)
-    })
-}
+  pool.query(
+    "INSERT INTO posts (id, title, content, image) VALUES ($1, $2, $3, $4) RETURNING *",
+    [id, title, content, image],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(201).send(`Post added with ID: ${results.rows[0].id}`);
+    }
+  );
+};
 
 const deletePost = (request, response) => {
-    const id = parseInt(request.params.id)
+  const id = parseInt(request.params.id);
 
-    pool.query('DELETE FROM posts WHERE id = $1', [id], (error, results) => {
-        if (error){
-            throw error
-        }
-        response.status(200).send(`Post deleted with ID: ${id}`)
-    })
-}
+  pool.query("DELETE FROM posts WHERE id = $1", [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).send(`Post deleted with ID: ${id}`);
+  });
+};
+
+const getUsers = (request, response) => {
+  pool.query("SELECT * FROM users ORDER BY id ASC", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+const getUserById = (request, response) => {
+  const id = parseInt(request.params.id);
+  pool.query("SELECT * FROM users WHERE id = $1", [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+
+const createUser = (request, response) => {
+  const { id, username, firstname, lastname, email, password } = request.body;
+
+  pool.query(
+    "INSERT INTO users (id, username, firstname, lastname, email, password) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *",
+    [id, username, firstname, lastname, email, password],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(201).send(`User added with ID: ${results.rows[0].id}`);
+    }
+  );
+};
+
+const deleteUser = (request, response) => {
+  const id = parseInt(request.params.id);
+
+  pool.query("DELETE FROM users WHERE id = $1", [id], (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).send(`User deleted with ID: ${id}`);
+  });
+};
 
 module.exports = {
-    getPosts,
-    getPostById,
-    createPost,
-    deletePost
-}
+  getPosts,
+  getPostById,
+  createPost,
+  deletePost,
+  getUsers,
+  getUserById,
+  createUser,
+  deleteUser,
+};
