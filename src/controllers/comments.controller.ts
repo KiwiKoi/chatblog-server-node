33be445822a9b50559617d5801d8fae485bd5ff5
Daniel <danielo.visage@gmail.com>
2023-10-08 10:57:21 +0200

    fix comment creation

--- src/controllers/comments.controller.ts
+++ src/controllers/comments.controller.ts
@@ -1,50 +1,51 @@
-import { Prisma, PrismaClient } from "@prisma/client";
-import { Request, Response } from "express";
+import {Prisma, PrismaClient} from "@prisma/client";
+import {Request, Response} from "express";
 
 const prisma = new PrismaClient();
 
 export function getComments() {
-  return async (req: Request, res: Response) => {
-    const { orderBy, postID } = req.query;
-    const comments = await prisma.comment.findMany({
-      orderBy: { created_at: orderBy as Prisma.SortOrder },
-      where: { postID: String(postID) },
-      include: { author: true },
-    });
-    res.json(comments);
-  };
+    return async (req: Request, res: Response) => {
+        const {orderBy, postID} = req.query;
+        const comments = await prisma.comment.findMany({
+            orderBy: {created_at: orderBy as Prisma.SortOrder},
+            where: {postID: String(postID)},
+            include: {author: true},
+        });
+        res.json(comments);
+    };
 }
 
 export function getCommentById() {
-  return async (req: Request, res: Response) => {
-    const { id }: { id?: string } = req.params;
-    const comment = await prisma.comment.findUnique({
-      where: { id: String(id) },
-      include: { author: true },
-    });
-    res.json(comment);
-  };
+    return async (req: Request, res: Response) => {
+        const {id}: { id?: string } = req.params;
+        const comment = await prisma.comment.findUnique({
+            where: {id: String(id)},
+            include: {author: true},
+        });
+        res.json(comment);
+    };
 }
 
 export function createComment() {
-  return async (req: Request, res: Response) => {
-    const { body, created_at, user_id, postID } = req.body;
-
-    const result = await prisma.comment.create({
-      data: { body, created_at, user_id, postID },
-    });
-    res.json(result);
-  };
+    return async (req: Request, res: Response) => {
+        const {body, created_at} = req.body;
+        const user_id = String(req.query.userId);
+        const postID = String(req.query.postId);
+        const result = await prisma.comment.create({
+            data: {body, created_at, user_id, postID},
+        });
+        res.json(result);
+    };
 }
 
 export function deleteComment() {
-  return async (req: Request, res: Response) => {
-    const { id } = req.params;
-    const comment = await prisma.comment.delete({
-      where: {
-        id: String(id),
-      },
-    });
-    res.json(comment);
-  };
+    return async (req: Request, res: Response) => {
+        const {id} = req.params;
+        const comment = await prisma.comment.delete({
+            where: {
+                id: String(id),
+            },
+        });
+        res.json(comment);
+    };
 }
